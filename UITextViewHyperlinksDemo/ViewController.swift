52a0d65f814223d7a4f143f366d63aecc69a04ee
Telolahy <stephano.telolahy@gmail.com>
2018-09-18 22:57:35 +0200

    initial commit

--- UITextViewHyperlinksDemo/ViewController.swift
+++ UITextViewHyperlinksDemo/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set these properties in interface builder
+        assert(textView.isSelectable)
+        assert(textView.dataDetectorTypes == .link)
+        
+        let html = """
+            <html>
+            <body>
+            <h1>Hello, go to <a href="http://www.google.com">google</a> and search for it</h1>
+            </body>
+            </html>
+            """
+        let data = Data(html.utf8)
+        guard let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) else {
+            return
+        }
+        textView.attributedText = attributedString
+        textView.delegate = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        // check for the url string for performing your own custom actions here
+        print("Interact with  \(URL.absoluteString)")
+        // Return NO if you don't want iOS to open the link
+        return true
     }
-
-
 }
 
